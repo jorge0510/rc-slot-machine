72276b4be852d69589eef2d01880487635d18487
Jorge Garcia <jagz0510@live.com>
2025-03-03 21:59:10 -0500

    slot machine working

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const reels = document.querySelectorAll(".innerScroller");
+const buttons = document.querySelectorAll("button");
+
+const symbols = ['cherry', 'blueberry', 'orange', 'bell', 'bar', 'lemon', 'watermelon', 'banana', 'seven'];
+
+let results;
+let tokens = 100;
+let bet = 0;
+
+const getRandomItem = () => symbols[Math.floor(Math.random() * symbols.length)];
+const didWin = () => results.every((i) => i === results[0]) && (tokens += bet * 1000) && alert('You Won!!')
+const stopButtons = () => buttons.forEach( item => item.disabled = true);
+const activateButtons = () => buttons.forEach( item => item.disabled = false);
+
+const getResults = () => { 
+    results = [];
+    reels.forEach( item => results.push(getRandomItem()));
+}
+
+const activateReels = () => {
+    reels.forEach(reelElement => {
+        reelElement.animate([
+            { transform: 'translateY(0%)' },
+            { transform: 'translateY(-100%)' }
+        ], {
+            duration: 1000,
+            iterations: 2
+        })
+    });
+}
+
+buttons.forEach( buttonNode => {
+    buttonNode.addEventListener('click', (e) => {
+        if (tokens < 0) { alert('you lost!'); return; }
+        e.target.classList.contains('minButton') ? bet = 1 : bet = 20;
+        e.target.classList.contains('minButton') ? tokens -= 1 : tokens -= 20;
+        document.querySelector('span').innerText = `YOU GOT ${tokens} TOKENS`;
+        stopButtons();
+        getResults();
+        activateReels();
+
+        setTimeout(() => {
+            reels.forEach( (reelElement, i) => {
+                reelElement.classList.remove(...symbols);
+                reelElement.classList.add(results[i]);
+            });
+            if (tokens > 0) {
+                didWin();
+            } else {
+                alert('you lost!');
+            }
+            activateButtons();
+        }, 2000)
+    });
+})
+
